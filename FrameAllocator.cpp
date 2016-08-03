f61d04e46175696129222f4135def4a72e004db9
Minh Nguyen 2 <Minh.Nguyen2@ubisoft.com>
2016-08-03 10:41:57 +0300

    added some basic methods

--- FrameAllocator.cpp
+++ FrameAllocator.cpp
@@ -1,12 +1,32 @@
 #include "FrameAllocator.h"
-
-
+#include <cstdlib>
+#include <exception>
+#include <cstdint>
 
 FrameAllocator::FrameAllocator(size_t size)
 {
+    void* mem = malloc(size);
+
+    if (!mem)
+        throw std::exception("Can't allocate memory");
+    
+    m_start = mem;
+    m_end = (uint8_t*)mem + size;
+    m_current = m_start;
 }
 
 
 FrameAllocator::~FrameAllocator()
 {
+    free(m_start);
+}
+
+void* FrameAllocator::allocate(size_t size, size_t alignment)
+{
+    return nullptr;
+}
+
+void FrameAllocator::reset()
+{
+    m_current = m_start;
 }
--- FrameAllocator.h
+++ FrameAllocator.h
@@ -5,8 +5,12 @@ public:
     FrameAllocator(size_t size);
     ~FrameAllocator();
 
+    void* allocate(size_t size, size_t alignment);
+
+    void reset();
 private:
     void* m_start;
     void* m_end;
+    void* m_current;
 };
 
