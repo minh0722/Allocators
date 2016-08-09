358c5092d39b5816be024c888c1a72a49ff107df
Minh Nguyen <minh0722@yahoo.com>
2016-08-09 09:58:37 +0300

    the utility scripts for diff and copying files

---
+++ copyFiles.sh
@@ -0,0 +1,9 @@
+FOLDER=F:\\playground\\assassin-playground\\ctsvetkov\\minh_nguyen\\Allocators\\Allocators
+
+cp \
+$FOLDER\\ImplicitFreeListAllocator.h \
+$FOLDER\\ImplicitFreeListAllocator.cpp \
+$FOLDER\\ExplicitFreeListAllocator.h \
+$FOLDER\\ExplicitFreeListAllocator.cpp \
+$FOLDER\\FrameAllocator.h \
+$FOLDER\\FrameAllocator.cpp .
---
+++ diff.sh
@@ -0,0 +1,3 @@
+FOLDER=F:\\playground\\assassin-playground\\ctsvetkov\\minh_nguyen\\Allocators\\Allocators
+
+diff $FOLDER\\$1 $1
